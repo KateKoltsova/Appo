da254e5f06d584a86b6eb41218fdf14e6a4960f3
Kate Koltsova <kate.petronyuk@gmail.com>
2023-09-29 23:31:31 +0300

    Make factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
+            'birthdate' => fake()->date(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'phone_number' => fake()->unique()->numerify('+380#########'),
+            'password' => fake()->password(3, 20),
         ];
     }
 

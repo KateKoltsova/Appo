809ea44ab5e3e57858841ce6ac938ceffd037caf
Kate Koltsova <kate.petronyuk@gmail.com>
2023-12-22 22:07:37 +0200

    Added categories to available schedules

--- app/Http/Resources/AvailableScheduleResource.php
+++ app/Http/Resources/AvailableScheduleResource.php
@@ -15,12 +15,17 @@ class AvailableScheduleResource extends JsonResource
     public function toArray(Request $request): array
     {
         if (!empty($this->schedules[0]) && !empty($this->prices[0])) {
+                foreach ($this->prices as $price) {
+                    $categoriesArray[] = $price['category'];
+                }
+            $uniqueCategories = array_unique($categoriesArray);
             $availableSchedule = [
                 'master_id' => $this->id,
                 'master_firstname' => $this->firstname,
                 'master_lastname' => $this->lastname,
                 'schedules' => ScheduleResource::collection($this->schedules),
-                'prices' => PriceResource::collection($this->prices)
+                'prices' => PriceResource::collection($this->prices),
+                'categories' => $uniqueCategories
             ];
         } else {
             $availableSchedule = [];

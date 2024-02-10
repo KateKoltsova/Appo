52da62e6a991667eb928917fdc0fe308f1eac001
Kate Koltsova <kate.petronyuk@gmail.com>
2024-02-11 01:00:44 +0200

    Refactored Users

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+
+class UserRepository
+{
+    public function __construct(protected User $model)
+    {
+    }
+
+    public function getAll($filters)
+    {
+        return $this->model->select([
+            'users.*',
+            'roles.role',
+        ])
+            ->join('roles', 'users.role_id', '=', 'roles.id')
+            ->when($filters['role'], function ($query) use ($filters) {
+                $query->whereIn('role', $filters['role']);
+            })
+            ->get();
+    }
+
+    public function getById(string $userId)
+    {
+        return $this->model->select([
+            'users.*',
+            'roles.role',
+        ])
+            ->join('roles', 'users.role_id', '=', 'roles.id')
+            ->where('users.id', $userId)
+            ->first();
+    }
+
+}

637850467c10de9ee68cf9a32737b8d2316a92ba
Kate Koltsova <64580045+KateKoltsova@users.noreply.github.com>
2024-10-17 13:37:52 +0000

    edit cart modal

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -2,8 +2,8 @@
 import {ref, watch} from 'vue';
 import {useRouter} from 'vue-router';
 import {useAuthWatcher, isAuthenticated} from '../localstorage';
-import {getCart, remove} from '../services/CartService';
 import LoadingSpinner from "./LoadingSpinner.vue";
+import CartModal from "./CartModal.vue";
 
 const token = ref('');
 const userId = ref('');
@@ -11,9 +11,7 @@ const router = useRouter();
 const iconClass = ref('fa-solid fa-right-to-bracket');
 
 const isLoading = ref(false);
-const cart = ref([]);
 const isCartModalOpen = ref(false);
-const hasErrorItems = ref(false);
 
 useAuthWatcher();
 
@@ -29,6 +27,7 @@ const checkUser = () => {
 };
 
 const handleClick = () => {
+    isCartModalOpen.value = false;
     if (isAuthenticated.value) {
         router.push('/profile');
     } else {
@@ -37,6 +36,7 @@ const handleClick = () => {
 };
 
 const handleBookingClick = () => {
+    isCartModalOpen.value = false;
     router.push('/booking');
 };
 
@@ -49,46 +49,7 @@ const handleStorageChange = () => {
 
 const handleCartClick = async () => {
     isCartModalOpen.value = !isCartModalOpen.value;
-    if (isCartModalOpen.value) {
-        await getCartItems();
-    }
-};
 
-const handleCheckoutClick = async () => {
-    isLoading.value = true;
-    isCartModalOpen.value = false;
-    router.push("/cart/checkout");
-    isLoading.value = false;
-};
-
-const getCartItems = async () => {
-    try {
-        isLoading.value = true;
-        const userId = localStorage.getItem("userId");
-        const response = await getCart(userId);
-        cart.value = response.data.data;
-        checkErrorItems();
-    } catch (error) {
-        console.error('Ошибка получения данных корзины:', error);
-    }
-    isLoading.value = false;
-};
-
-const removeItem = async (itemId) => {
-    try {
-        isLoading.value = true;
-        const userId = localStorage.getItem("userId");
-        await remove(userId, itemId);
-        cart.value.items = cart.value.items.filter((item) => item.id !== itemId);
-        checkErrorItems();
-    } catch (error) {
-        console.error("Ошибка удаления элемента:", error);
-    }
-    isLoading.value = false;
-};
-
-const checkErrorItems = () => {
-    hasErrorItems.value = cart.value.items.some(item => item.message);
 };
 </script>
 
@@ -109,40 +70,7 @@ const checkErrorItems = () => {
             </div>
         </nav>
     </header>
-    <div v-if="isCartModalOpen" class="cart-modal">
-        <div class="cart-modal-content">
-            <h2>Корзина</h2>
-            <ul>
-                <li v-for="item in cart.items" :key="item.id">
-                    <div class="cart-item-card" :class="{'error-item': item.message}">
-                        <h3>{{ item.title }}</h3>
-                        <p class="item-info">Дата и время: {{ item.date_time }}</p>
-                        <p class="item-info">Мастер: {{ item.master_firstname }} {{ item.master_lastname }}</p>
-                        <p class="item-info">Цена: {{ item.price }} ГРН</p>
-
-                        <button class="remove-button" @click="removeItem(item.id)">✖</button>
-
-                        <div v-if="item.message" class="error-message">
-                            Это время больше недоступно! Удалите его, чтобы продолжить!
-                        </div>
-                    </div>
-                </li>
-            </ul>
-
-            <div class="cart-summary">
-                <p>Общее количество: {{ cart.totalCount }}</p>
-                <p>Итоговая сумма: {{ cart.totalSum }} ГРН</p>
-            </div>
-
-            <button
-                class="checkout-button"
-                :disabled="hasErrorItems"
-                @click="handleCheckoutClick"
-            >
-                Оформить заказ
-            </button>
-        </div>
-    </div>
+    <CartModal :isCartModalOpen="isCartModalOpen" />
 </template>
 
 <style scoped>
@@ -191,82 +119,5 @@ const checkErrorItems = () => {
 
 .booking-button:hover {
     background-color: #218838;
-}
-
-.cart-modal {
-    position: fixed;
-    top: 0;
-    right: 0;
-    width: 50%;
-    height: 100%;
-    background-color: #fff;
-    z-index: 1000;
-    box-shadow: -2px 0 5px rgba(0, 0, 0, 0.5);
-    padding: 20px;
-}
-
-.cart-modal-content {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-}
-
-.cart-item-card {
-    border: 1px solid #ccc;
-    padding: 10px;
-    margin-bottom: 10px;
-    border-radius: 5px;
-    position: relative;
-}
-
-.error-item {
-    border-color: red;
-    background-color: #ffe6e6;
-}
-
-.item-info {
-    font-size: 14px;
-    margin-bottom: 5px;
-}
-
-.error-message {
-    color: red;
-    font-size: 12px;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    padding: 5px;
-    background-color: rgba(255, 0, 0, 0.2);
-    text-align: center;
-}
-
-.remove-button {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    background: none;
-    border: none;
-    font-size: 20px;
-    cursor: pointer;
-}
-
-.cart-summary {
-    text-align: right;
-    margin-top: 20px;
-}
-
-.checkout-button {
-    background-color: #28a745;
-    border: none;
-    padding: 15px;
-    color: #fff;
-    font-size: 18px;
-    cursor: pointer;
-    width: 100%;
-}
-
-.checkout-button:disabled {
-    background-color: grey;
 }
 </style>
